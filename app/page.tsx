caf627156f9d99211da6d50a8121b768665d0f73
xicasal <xicasal17@esei.uvigo.es>
2024-12-20 16:44:23 +0100

    mas y mas cambios

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 import { getServerSession } from 'next-auth'
-import { authOptions } from './api/auth/[...nextauth]/route'
 import { redirect } from 'next/navigation'
 import MainApp from '@/components/MainApp'
+import { authOptions } from '@/lib/authOptions'
 
 
 
--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -1,8 +1,8 @@
 
 import { getServerSession } from 'next-auth'
-import { authOptions } from '../api/auth/[...nextauth]/route'
 import { redirect } from 'next/navigation'
 import ShowAndPickProfile from '@/components/ShowProfile'
+import { authOptions } from '@/lib/authOptions'
 
 export default async function ProfilesPage() {
 
