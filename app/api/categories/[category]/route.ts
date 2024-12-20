5d7fe914478d0a1673298e2c78184615002e1c84
xicasal <xicasal17@esei.uvigo.es>
2024-12-20 17:13:17 +0100

    otra prueba

--- app/api/categories/[category]/route.ts
+++ app/api/categories/[category]/route.ts
@@ -2,28 +2,31 @@ import { NextResponse } from 'next/server'
 import prismadb from '@/lib/prismadb'
 
 export async function GET(
-  _: unknown,
-  params : { params: { category: string } }
+  request: Request,
+  { params }: { params: { category: string } }
 ) {
-  
-  const category = params
+  const { category } = params
 
-  if (!category) {
-    return NextResponse.json({ message: 'Id no válido' }, { status: 404 })
+  if (!category || typeof category !== 'string') {
+    return NextResponse.json({ message: 'Categoría no válida' }, { status: 400 })
   }
 
   try {
     const movies = await prismadb.movies.findMany({
       where: {
         genres: {
-          has: category?.params.category,
+          has: category, // Busca películas que contengan la categoría en el array 'genres'
         },
       },
     })
 
+    if (movies.length === 0) {
+      return NextResponse.json({ message: 'No se encontraron películas para esta categoría' }, { status: 404 })
+    }
+
     return NextResponse.json(movies, { status: 200 })
   } catch (error) {
     console.error('Error al obtener las películas:', error)
-    return NextResponse.json({ message: 'Error del servidor' }, { status: 500 })
+    return NextResponse.json({ message: 'Error interno del servidor' }, { status: 500 })
   }
 }
